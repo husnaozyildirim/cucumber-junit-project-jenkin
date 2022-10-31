b895c9cde5e1fd8d82c4de17de50189c0a82edbd
Husna <ozyildirimhusna@gmail.com>
2022-10-31 13:00:26 -0400

    WebtableApp

--- src/test/java/com/cydeo/pages/BasePage.java
+++ src/test/java/com/cydeo/pages/BasePage.java
@@ -3,8 +3,8 @@ package com.cydeo.pages;
 import com.cydeo.utilities.Driver;
 import org.openqa.selenium.support.PageFactory;
 
-public class BaseWebpage {
-    public BaseWebpage(){
+public class BasePage {
+    public BasePage(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
 }
