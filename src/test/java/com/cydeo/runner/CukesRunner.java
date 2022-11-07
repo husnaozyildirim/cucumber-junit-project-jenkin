442f0950688296ef3e8f98633de75b80682a1b95
Husna <ozyildirimhusna@gmail.com>
2022-11-07 16:39:30 -0500

    add json

--- src/test/java/com/cydeo/runner/CukesRunner.java
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin =  { "pretty" ,
+                "json:target/cucumber.json" ,
                 "html:target/cucumber-reports.html" ,
                 "rerun:target/rerun.txt",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber"
