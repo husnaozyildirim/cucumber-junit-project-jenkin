4568146b7602616625b631c859371b589c29a161
Husna <ozyildirimhusna@gmail.com>
2022-11-10 16:07:18 -0500

    "run in SG"

--- src/test/java/com/cydeo/runner/CukesRunner.java
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/feature" ,
         glue = "com/cydeo/step_definitions" ,
         dryRun = false ,
-        tags =  "@smoke",
+        tags =  "",
         publish = true
 )
 public class CukesRunner { }
